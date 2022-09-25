fc9c63db2e672630cc80c74c968011731e305dc9
Faisal Ahmed <faisal2410@yahoo.com>
2022-09-25 16:32:29 +0600

    updated

--- __note.js
+++ __note.js
@@ -4,5 +4,21 @@
  * 
  * https://simple-grocery-store-api.glitch.me/
  * 
+ * 1.Status
+ * GET /status
  * 
+ *2. Get all products
+   GET /products
+ * 
+ * 3.Get Single product
+   GET /products/:productId
+   4.Get a cart
+GET /carts/:cartId
+5.Get cart items
+Returns the items in a cart.
+GET /carts/:cartId/items
+
+6.Create a new cart
+To create a new cart, submit an empty POST request to the /carts endpoint.
+POST /carts
  * */ 
