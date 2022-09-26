bb3525f761610f31e8e615826f8067968799f88c
Faisal Ahmed <faisal2410@yahoo.com>
2022-09-26 20:15:26 +0600

    updated in the class

--- __note.js
+++ __note.js
@@ -4,6 +4,35 @@
  * 
  * https://simple-grocery-store-api.glitch.me/
  * 
+ * Postman Reference Guide :
+ * https://postman-quick-reference-guide.readthedocs.io/en/latest/
+ * 
+ * Understanding Javascript Callback
+ * 
+ * http://javascriptissexy.com/understand-javascript-callback-functions-and-use-them/
+ * 
+ * Postman Echo system
+ * https://www.postman.com/postman/workspace/published-postman-templates/documentation/631643-f695cab7-6878-eb55-7943-ad88e1ccfd65?ctx=documentation
+ * 
+ * Swagger
+ * https://swagger.io/tools/swaggerhub/
+ * 
+ * Designing and developing api
+ * 
+ * https://learning.postman.com/docs/designing-and-developing-your-api/the-api-workflow/
+ * 
+ * Postman Webinars
+ * https://www.youtube.com/watch?v=iDJjwT0X2s0
+ * 
+ * https://www.youtube.com/playlist?list=PL6iUkDSEH9St3CkMTY_xbDAUIK58GCfsb
+ * 
+ * 
+ * Official details about the challenge can be found here:
+https://blog.postman.com/introducing-30-days-of-postman-coding-challenge/
+
+https://www.postman.com/postman/workspace/30-days-of-postman-for-developers/overview
+
+
  * 1.Status
  * GET /status
  * 
@@ -21,4 +50,11 @@ GET /carts/:cartId/items
 6.Create a new cart
 To create a new cart, submit an empty POST request to the /carts endpoint.
 POST /carts
+ * */ 
+
+/**
+ * https://www.google.com/search?q=udemy&rlz=1C1CHBD_enBD933BD933&oq=udemy&aqs=chrome..69i57j0i271l2j69i60j69i65j69i60l2.4002j0j1&sourceid=chrome&ie=UTF-8
+ * 
+ * 
+ * 
  * */ 
---
+++ __trelloNote.js
@@ -0,0 +1,9 @@
+/**
+ * https://developer.atlassian.com/cloud/trello/guides/rest-api/api-introduction/#your-first-api-call
+ * 
+ * https://developer.atlassian.com/cloud/trello/rest/api-group-boards/#api-boards-id-memberships-get
+ * 
+ * https://developer.atlassian.com/cloud/trello/guides/rest-api/api-introduction/
+ * 
+ * 
+ * */ 
